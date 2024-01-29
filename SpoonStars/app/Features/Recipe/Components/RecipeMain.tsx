57b03c26f4773803f1fcdb8e8dfec7c39eae2e1b
Mark John Alcantara <MarkAlcantara@Aprils-MBP.ht.home>
2024-01-28 20:10:05 -0500

    closes #114

--- SpoonStars/app/Features/Recipe/Components/RecipeMain.tsx
+++ SpoonStars/app/Features/Recipe/Components/RecipeMain.tsx
@@ -1,72 +1,42 @@
-import React, { FC, useEffect, ReactElement, useState, useRef } from 'react';
+import React, { FC, useEffect, useState, useRef } from 'react';
 import { useAppSelector, useAppDispatch } from '../../../Redux/Hooks';
-import { selectConfig, selectConfigStatus } from '../../Configuration/ConfigSlice';
-import { selectRecipes, recipePayload, fetchRecipes, selectRecipesStatus, 
-  selectRecipesPageInfo, selectRecipeErrors, clearRecipes, selectRecipeTags, 
+import { selectConfigStatus } from '../../Configuration/ConfigSlice';
+import { selectRecipes, recipePayload, selectRecipesStatus, 
+  selectRecipesPageInfo, clearRecipes, selectRecipeTags, 
   updateRecipePreference, saveRecipePreference, selectRecipePreferencesStatus } from '../Scripts/RecipeSlice';
-import RecipeItem from './RecipeItem';
-import { GestureHandlerRootView, RefreshControl, ScrollView } from 'react-native-gesture-handler';
-import { UIActivityIndicator } from 'react-native-indicators';
+import { GestureHandlerRootView, ScrollView } from 'react-native-gesture-handler';
 import { Dimensions, StatusBar, StyleSheet, View } from 'react-native';
 import { Button, TextInput } from 'react-native-paper';
 import { ErrorMain } from '../../Error/ErrorMain';
-import { FlashList } from '@shopify/flash-list';
 import { selectConfigError } from '../../Configuration/ConfigSlice';
 import loading from '../../Shared/Components/Loading';
-import { HomeScreenProps, Suggestions } from './../../../../types/App_Types';
+import { HomeScreenProps } from './../../../../types/App_Types';
 import { useAppTheme } from './../../../App';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { StackNavigation } from './../../../../types/App_Types';
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import useFeed from '../Scripts/useFeed';
 import RecipeFeed from './RecipeFeed';
 import { recipeApi } from '../../Api/RecipeApi';
 
 
 const RecipeMain: FC<HomeScreenProps> = ( {navigation, route}: HomeScreenProps ) => {
   const { colors: { primary } } = useAppTheme();
-  const recipesState: recipePayload[] = useAppSelector(selectRecipes);
+  
   const recipeTags = useAppSelector(selectRecipeTags);
   const preferenceStatus = useAppSelector(selectRecipePreferencesStatus);
-  const [refreshing, setRefreshing] = useState(false);
   const [tagStyles, setTagStyles] = useState<string[]>([]);
-  const flashList = useRef(null);
   const { RecipeLoader } = loading();
   const { navigate } = useNavigation<StackNavigation>();
   const autocompleteField = useRef<any>(null);
   
-  const configState = useAppSelector(selectConfig);
   const configStatusState = useAppSelector(selectConfigStatus);
-  const recipeStatusState = useAppSelector(selectRecipesStatus);
-  const recipePageInfo = useAppSelector(selectRecipesPageInfo);
-  const recipeStateErrors = useAppSelector(selectRecipeErrors);
   const configStateErrors: string = useAppSelector(selectConfigError);
   const dispatch = useAppDispatch();
 
-  const renderItem = ({item}:{
-    item: Suggestions;
-    index?: number;
-  }): ReactElement => {
-    return (
-      <RecipeItem item={item}  />
-    );
-  };
-
-  // useEffect(() => {
-  //   if (configStatusState === 'succeeded'){
-  //     dispatch(fetchRecipes());
-  //   }
-  // }, [configState])
   
-  useEffect(() => {
-  }, [recipesState])
 
-  const loadMore = async ()=> {
-    if ((recipeStatusState === 'succeeded' || recipeStatusState === 'idle') && recipePageInfo.hasNextPage) {
-      await dispatch(fetchRecipes());
-    }
-  }
+  
 
   useEffect(() => {
     if (preferenceStatus === 'succeeded') {
@@ -75,18 +45,6 @@ const RecipeMain: FC<HomeScreenProps> = ( {navigation, route}: HomeScreenProps )
     }
   }, [preferenceStatus]);
 
-  // const footer = () => {
-  //   if (recipeStatusState !== 'loading') 
-  //     return null;
-  //   return (<UIActivityIndicator size={30} />)
-  // }
-
-  const handleRefresh = async () => {
-    setRefreshing(true);
-    dispatch(clearRecipes());
-    await dispatch(fetchRecipes());
-    setRefreshing(false);
-  }
   
   const createPreferenceOptions = () => {
     return ( 
@@ -125,10 +83,6 @@ const RecipeMain: FC<HomeScreenProps> = ( {navigation, route}: HomeScreenProps )
     autocompleteField.current.blur();
     navigate('Search');
   }
-
-  // if (configStatusState !== 'succeeded' ||  preferenceStatus !== 'succeeded') {
-  //       return (<></>);
-  // }
 
   if (configStateErrors !== '') {
     return (<ErrorMain message={configStateErrors} />);
